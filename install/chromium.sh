5949886a0ee04896c6efa910cd68f7cdd19fb9c0
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-08 11:33:44 +0000

    fix: chromium is under snap by default, no snap

--- install/chromium.sh
+++ install/chromium.sh
@@ -1,2 +1,4 @@
 # Browse the web with the most popular browser. See https://www.google.com/chrome/
-sudo apt install -y chromium-browser
+sudo add-apt-repository -y ppa:xtradeb/apps
+sudo apt update -y
+sudo apt install -y chromium
