73ff50efea421fbeb123a3570b3779267a3224ec
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-08 11:23:23 +0000

    fix: use greetd without plugins

--- install/6-sway.sh
+++ install/6-sway.sh
@@ -2,7 +2,7 @@
 sudo apt install -y \
   sway swaybg swaylock \
   waybar wofi \
-  network-manager greeted greetd-config greetd-ui
+  network-manager greetd
 
 # Start Sway on first session
 echo "[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec sway" >~/.bash_profile
