7b5dc7474338db4125e8057b2d9888a230e12d29
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-09 08:04:24 +0200

    upd: optiong for ubuntu server, no deboostrap needed

--- install/5-sway.sh
+++ install/5-sway.sh
@@ -2,7 +2,7 @@
 sudo apt install -y \
   sway swaybg swaylock \
   waybar wofi \
-  network-manager greetd
+  network-manager
 
 # Start Sway on first session
 echo "[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec sway" >~/.bash_profile
