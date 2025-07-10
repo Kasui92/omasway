8b313428ade2221f42a4176e326c96aba5da75b6
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-10 16:34:46 +0200

    upd: improve sway configs

--- install/5-sway.sh
+++ install/5-sway.sh
@@ -1,4 +1,5 @@
 # Sway - A Wayland compositor
 sudo apt install -y \
-  sway swaybg swaylock \
-  waybar wofi mako iwd
+  sway swaybg swaylock polkit-gnome\
+  waybar wofi mako iwd \
+  xdg-desktop-portal-wlr xdg-desktop-portal-gtk
