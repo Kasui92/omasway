5136463d38bf8c3b29354e311837f63a169aa47c
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-10 16:57:58 +0200

    fix: typo in background

--- install/backgrounds.sh
+++ install/backgrounds.sh
@@ -1,6 +1,6 @@
 BACKGROUNDS_DIR=~/.config/omasway/backgrounds/
 
-downloa_background_image() {
+download_background_image() {
   local url="$1"
   local path="$2"
   gum spin --title "Downloading $url as $path..." -- curl -sL -o "$BACKGROUNDS_DIR/$path" "$url"
