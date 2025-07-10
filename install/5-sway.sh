154062f01e2a6f7057dcfa749557d403401a04d8
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-10 16:55:48 +0200

    upd: add sway libraries

--- install/5-sway.sh
+++ install/5-sway.sh
@@ -1,5 +1,6 @@
 # Sway - A Wayland compositor
 sudo apt install -y \
-  sway swaybg swaylock polkit-gnome\
-  waybar wofi mako iwd \
-  xdg-desktop-portal-wlr xdg-desktop-portal-gtk
+  sway swaybg swaylock swayidle mate-polkit-bin \
+  waybar wofi mako-notifier iwd \
+  xwayland wayland-protocols wayland-utils xdg-desktop-portal-wlr xdg-desktop-portal-gtk \
+  qtwayland5 qt6-wayland
