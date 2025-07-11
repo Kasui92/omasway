e924fa287d92c6a37f3d085859aebbe4af1d5580
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-11 10:46:57 +0200

    fix: remove iwd (#2)

--- install/5-sway.sh
+++ install/5-sway.sh
@@ -1,9 +1,6 @@
 # Sway - A Wayland compositor
 sudo apt install -y \
   sway swaybg swaylock swayidle mate-polkit-bin \
-  waybar wofi mako-notifier iwd \
+  waybar wofi mako-notifier \
   xwayland wayland-protocols wayland-utils xdg-desktop-portal-wlr xdg-desktop-portal-gtk \
   qtwayland5 qt6-wayland
-
-# Enable iwd for Wi-Fi management
-sudo systemctl enable iwd
