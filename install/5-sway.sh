463deaa60a13763c3b8a048a2f679b1d91693047
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-14 00:49:37 +0200

    upd: use autotiling specific version

--- install/5-sway.sh
+++ install/5-sway.sh
@@ -9,7 +9,10 @@ sudo apt install -y \
 sudo apt install -y python3-i3ipc
 
 cd /tmp
-wget -qO- https://raw.githubusercontent.com/nwg-piotr/autotiling/autotiling/main.py > autotiling
-mv autotiling ~/.local/bin/autotiling
+AUTOTILING_VERSION=$(curl -s "https://api.github.com/repos/nwg-piotr/autotiling/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -sLo autotiling.tar.gz "https://github.com/nwg-piotr/autotiling/archive/refs/tags/${AUTOTILING_VERSION}.tar.gz"
+tar -xf autotiling.tar.gz autotiling
+mv autotiling/autotiling/main.py ~/.local/bin/autotiling
 chmod +x ~/.local/bin/autotiling
+rm autotiling.tar.gz autotiling
 cd -
