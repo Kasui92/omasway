977e9e488e09a6ea42d083eae1bcb082c6493b0f
Luca Pattocchio <pattocchioluca@gmail.com>
2025-07-14 00:37:41 +0200

    upd: add autotiling for sway

--- install/5-sway.sh
+++ install/5-sway.sh
@@ -4,3 +4,12 @@ sudo apt install -y \
   waybar wofi mako-notifier \
   xwayland wayland-protocols wayland-utils xdg-desktop-portal-wlr xdg-desktop-portal-gtk \
   qtwayland5 qt6-wayland
+
+# Autotiling - a tiling script for Sway
+sudo apt install -y python3-i3ipc
+
+cd /tmp
+wget -qO- https://raw.githubusercontent.com/nwg-piotr/autotiling/autotiling/main.py > autotiling
+mv autotiling ~/.local/bin/autotiling
+chmod +x ~/.local/bin/autotiling
+cd -
